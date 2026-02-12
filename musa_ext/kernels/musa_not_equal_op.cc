fcafd4cd5ba1dc9e79fe9359b00bbf24ae566e94
albert <albert@mthreads.com>
2026-02-12 10:19:52 +0800

    init

---
+++ musa_ext/kernels/musa_not_equal_op.cc
@@ -0,0 +1,16 @@
+#include "utils_op.h"
+
+namespace tensorflow {
+namespace musa {
+ 
+template <typename T>
+class MusaNotEqualOp : public MusaOpKernel {
+    public:
+
+    void Compute(OpKernelContext* ctx){
+        
+    }
+}
+
+}
+}
---
+++ test/not_equal_op_test.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+import tensorflow as tf
+
+from musa_test_utils import MUSATestCase
+
+class NotEqualOpTest(MUSATestCase):
+    """Test cases for NotEqualOp"""
+    
+    def _test
