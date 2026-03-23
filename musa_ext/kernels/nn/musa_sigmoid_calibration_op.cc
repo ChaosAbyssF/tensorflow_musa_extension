906ada7dc0214c8a0ed8f1d365007c354801042c
ronnie <ronnie@mt.com>
2026-03-23 11:07:41 +0800

    use kernel

--- musa_ext/kernels/nn/musa_sigmoid_calibration_op.cc
+++ musa_ext/kernels/nn/musa_sigmoid_calibration_op.cc
@@ -39,7 +39,8 @@ class MusaSigmoidCalibrationOp : public MusaOpKernel {
     Tensor* output;
     OP_REQUIRES_OK(ctx, ctx->allocate_output(0, input.shape(), &output));
 
-    useMudnn(ctx, input, scale, output);
+    // useMudnn(ctx, input, scale, output);
+    useKernel(ctx, input, scale, output);
   }
 
  private:
@@ -57,8 +58,8 @@ class MusaSigmoidCalibrationOp : public MusaOpKernel {
                 Tensor* output) {
     // 1. Run Sigmoid
     Tensor sigmoid_tensor;
-    OP_REQUIRES_OK(ctx, ctx->allocate_temp(input.dtype(), input.shape(),
-                         &sigmoid_tensor));
+    OP_REQUIRES_OK(
+        ctx, ctx->allocate_temp(input.dtype(), input.shape(), &sigmoid_tensor));
     auto sigmoid_mt = CreateMTensor(sigmoid_tensor, format_);
     auto& handle = GetHandleByCtx(ctx);
     auto in_mt = CreateMTensor(input, format_);
@@ -71,15 +72,15 @@ class MusaSigmoidCalibrationOp : public MusaOpKernel {
 
     // 2. Run 1 - S
     Tensor one_tensor;
-    OP_REQUIRES_OK(ctx, ctx->allocate_temp(input.dtype(), input.shape(),
-                         &one_tensor));
+    OP_REQUIRES_OK(
+        ctx, ctx->allocate_temp(input.dtype(), input.shape(), &one_tensor));
     auto one_mt = CreateMTensor(one_tensor, format_);
     mFill fill_op;
     MTOP_CHECK_OK(fill_op.SetValue(1.0f), "Set Fill Value", ctx);
     MTOP_CHECK_OK(fill_op.Run(handle, one_mt), "Fill One Tensor", ctx);
     Tensor one_minus_s_tensor;
     OP_REQUIRES_OK(ctx, ctx->allocate_temp(input.dtype(), input.shape(),
-                         &one_minus_s_tensor));
+                                           &one_minus_s_tensor));
     auto one_minus_s_mt = CreateMTensor(one_minus_s_tensor, format_);
     ::musa::dnn::Binary sub_op;
     MTOP_CHECK_OK(sub_op.SetMode(::musa::dnn::Binary::Mode::SUB), "Set Sub",
@@ -90,21 +91,21 @@ class MusaSigmoidCalibrationOp : public MusaOpKernel {
     // 3. Run Scale * (1 - S)
     Tensor scale_times_one_minus_s_tensor;
     OP_REQUIRES_OK(ctx, ctx->allocate_temp(input.dtype(), input.shape(),
-                         &scale_times_one_minus_s_tensor));
+                                           &scale_times_one_minus_s_tensor));
     auto scale_times_one_minus_s_mt =
-      CreateMTensor(scale_times_one_minus_s_tensor, format_);
+        CreateMTensor(scale_times_one_minus_s_tensor, format_);
     auto scale_mt = CreateMTensor(scale, format_);
     ::musa::dnn::Binary mul_op;
     MTOP_CHECK_OK(mul_op.SetMode(::musa::dnn::Binary::Mode::MUL), "Set Mul",
                   ctx);
-    MTOP_CHECK_OK_RUN(
-        mul_op.Run(handle, scale_times_one_minus_s_mt, scale_mt, one_minus_s_mt),
-        "Scale Times One Minus S Run", ctx);
+    MTOP_CHECK_OK_RUN(mul_op.Run(handle, scale_times_one_minus_s_mt, scale_mt,
+                                 one_minus_s_mt),
+                      "Scale Times One Minus S Run", ctx);
 
     // 4. Run S + Scale * (1 - S)
     Tensor denominator_tensor;
     OP_REQUIRES_OK(ctx, ctx->allocate_temp(input.dtype(), input.shape(),
-                         &denominator_tensor));
+                                           &denominator_tensor));
     auto denominator_mt = CreateMTensor(denominator_tensor, format_);
     ::musa::dnn::Binary add_op;
     MTOP_CHECK_OK(add_op.SetMode(::musa::dnn::Binary::Mode::ADD), "Set Add",
